d4842c78a5e6e9b11995e12c22c27de1ab1f0f47
DizzySlo <dizzyslo@bk.ru>
2021-08-18 09:05:31 +0300

    Правки

--- src/com/company/ServicePrice.java
+++ src/com/company/ServicePrice.java
@@ -1,12 +1,14 @@
 package com.company;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.math.BigDecimal;
 
 public class ServicePrice {
   private final BigDecimal price;
   private final String currency;
 
-  public ServicePrice(BigDecimal price, String currency) {
+  public ServicePrice(@NotNull BigDecimal price, @NotNull String currency) {
     this.price = price;
     this.currency = currency;
   }
